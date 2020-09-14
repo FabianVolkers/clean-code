55f18d6fbcb6f67dd9406aec422305e83aa99eeb
FabianVolkers <fabian.volkers@gmail.com>
2020-09-14 13:11:40 +0000

    remove redundant imports

--- FilePath.py
+++ FilePath.py
@@ -1,5 +1,3 @@
-import re
-import random
 
 def check_path(filename):
     try:
@@ -19,42 +17,36 @@ def get_dir(filename):
     dirName = ''
     if index >= 0:
         dirName = filename[0: index + 1]
-    else:
-        dirName = ''
 
     return dirName
 
-# access_log
-def getFilenamePart(sFilename):
+def get_filename(filename):
+
+    index = check_path(filename)
+    file_name = ''
+    if index >= 0:
+        file_name = filename[index + 1:]
+        return file_name
+    else:
+        return filename
+
+
+
+def get_file_ext(filename):
     try:
-        int(sFilename.rindex('/'))
+        return filename[filename.rindex('.')+1:]
     except:
-        return sFilename
-
-    pos = sFilename.rindex('/')
-    base_name = sFilename[pos + 1:]
-    return base_name
-
-
-#.png
-def getEndOfFile(sFilename):
-    try:
-        occurrences = [m.start() for m in re.finditer('\.', sFilename)]
-        return sFilename[occurrences[-1] + 1:]
-    except:
-        pass
-
-    return ''
+        return ''
 
 
 assert(get_dir("log/cups/access_log") == "log/cups/")
 assert(get_dir("log/cups/") == "log/cups/")
 assert(get_dir("cups/access_log") == "cups/")
 assert(get_dir("access_log") == "")
-assert(getFilenamePart("log/cups/access_log") == "access_log")
-assert(getFilenamePart("log/cups/") == "")
-assert(getFilenamePart("cups/access_log") == "access_log")
-assert(getFilenamePart("access_log") == "access_log")
-assert(getEndOfFile("log/cups/access_log") == "")
-assert(getEndOfFile("base/FileHelper.cpp") == "cpp")
-assert(getEndOfFile("base/FileHelper.cpp.bak") == "bak")
+assert(get_filename("log/cups/access_log") == "access_log")
+assert(get_filename("log/cups/") == "")
+assert(get_filename("cups/access_log") == "access_log")
+assert(get_filename("access_log") == "access_log")
+assert(get_file_ext("log/cups/access_log") == "")
+assert(get_file_ext("base/FileHelper.cpp") == "cpp")
+assert(get_file_ext("base/FileHelper.cpp.bak") == "bak")
